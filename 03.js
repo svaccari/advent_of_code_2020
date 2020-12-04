bb14a69aeca61a4a326850450746418074c982c2
s.vaccari <s.vaccari@ebw.it>
2020-12-04 22:29:56 +0100

    Three down

--- 03.js
+++ 03.js
@@ -1,6 +1,26 @@
-var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('03.txt')
-});
+const { readFileSync } = require('fs')
 
-lineReader.on('line', function (line) {
-});
+const data = readFileSync('03.txt').toString().replace(/\r\n/g, '\n').split('\n')
+
+let w = data[0].length
+
+let slopes = [
+    { offsetX: 1, offsetY: 1, trees: 0 },
+    { offsetX: 3, offsetY: 1, trees: 0 },
+    { offsetX: 5, offsetY: 1, trees: 0 },
+    { offsetX: 7, offsetY: 1, trees: 0 },
+    { offsetX: 1, offsetY: 2, trees: 0 }
+]
+
+let mult = 1
+for (let s of slopes) {
+    let x = 0
+    for (let y = 0; y < data.length; y += s.offsetY) {
+        s.trees += data[y][x % w] === '#' ? 1 : 0
+        x += s.offsetX
+    }
+    console.log(`Trees = ${s.trees}`)
+    mult *= s.trees
+}
+
+console.log(`Multiplication of trees = ${mult}`)
