a4a6bc960a03945248688f74eca1bc953f3ef92a
s.vaccari <s.vaccari@ebw.it>
2020-12-22 21:47:50 +0100

    22 fixed

--- 22.js
+++ 22.js
@@ -1,6 +1,13 @@
 let p1 = [7, 1, 9, 10, 12, 4, 38, 22, 18, 3, 27, 31, 43, 33, 47, 42, 21, 24, 50, 39, 8, 6, 16, 46, 11]
 let p2 = [49, 41, 40, 35, 44, 29, 30, 19, 14, 2, 34, 17, 25, 5, 15, 32, 20, 48, 45, 26, 37, 28, 36, 23, 13]
 
+function score(a) {
+    let output = 0
+    for (let i = 0; i < a.length; i++)
+        output += (a.length - i) * a[i]
+    return output
+}
+
 while (p1.length && p2.length) {
     let card1 = p1.shift()
     let card2 = p2.shift()
@@ -13,31 +20,22 @@ while (p1.length && p2.length) {
     }
 }
 
-console.log(p1.length, p2.length)
-
-let score = 0
-
-for (let i = 0; i < p1.length; i++)
-    score += (p1.length - i) * p1[i]
-
-console.log('Score', score)
+console.log('Score', score(p1), score(p2))
 
 // Part two
 
 p1 = [7, 1, 9, 10, 12, 4, 38, 22, 18, 3, 27, 31, 43, 33, 47, 42, 21, 24, 50, 39, 8, 6, 16, 46, 11]
 p2 = [49, 41, 40, 35, 44, 29, 30, 19, 14, 2, 34, 17, 25, 5, 15, 32, 20, 48, 45, 26, 37, 28, 36, 23, 13]
 
-let played = []
-function game(a, b) {
+function game(a, b, played) {
     if (b.length === 0)
         return 1
     if (a.length === 0)
         return 2
-    let curr = a.join(',') + '|' + b.join(',')
+    let curr = score(a) * 10e10 + score(b)
     if (played.includes(curr))
         return 1
     played.push(curr)
-
     let card1 = a.shift()
     let card2 = b.shift()
     if (a.length >= card1 && b.length >= card2) {
@@ -46,7 +44,7 @@ function game(a, b) {
             aCopy.push(a[i])
         for (let i = 0; i < card2; i++)
             bCopy.push(b[i])
-        let winner = game(aCopy, bCopy)
+        let winner = game(aCopy, bCopy, [])
         if (winner === 1) {
             a.push(card1)
             a.push(card2)
@@ -63,26 +61,9 @@ function game(a, b) {
             b.push(card1)
         }
     }
-    if (b.length === 0)
-        return 1
-    if (a.length === 0)
-        return 2
-    return game(a, b)
+    return game(a, b, played)
 }
 
-game(p1, p2)
+game(p1, p2, [])
 
-console.log(p1.length, p2.length)
-
-score = 0
-
-if (p1.length) {
-    for (let i = 0; i < p1.length; i++)
-        score += (p1.length - i) * p1[i]
-}
-if (p2.length) {
-    for (let i = 0; i < p2.length; i++)
-        score += (p2.length - i) * p2[i]
-}
-
-console.log('Score part two', score) // 32609, 4661
+console.log('Score part two', score(p1), score(p2))
