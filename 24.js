90b8609c7a0d76494d73426a783d51a41b54776c
s.vaccari <s.vaccari@ebw.it>
2020-12-24 14:16:28 +0100

    24 done

--- 24.js
+++ 24.js
@@ -2,7 +2,15 @@ const { readFileSync } = require('fs')
 
 const data = readFileSync(__filename.replace('js', 'txt')).toString().replace(/\r\n/g, '\n').split('\n')
 
-let tiles = {}
+function getHash(x, y) {
+    return (y + 1000) * 10e6 + (x + 1000)
+}
+
+function getXY(hash) {
+    return [hash % 10e6 - 1000, Math.floor(hash / 10e6) - 1000]
+}
+
+let tiles = new Map()
 for (let d of data) {
     let moves = [] // e, se, sw, w, nw, ne
     let curr = ''
@@ -16,22 +24,58 @@ for (let d of data) {
         } else
             curr += c
     }
-    let offset = [0, 0]
+    let x = 0, y = 0
     for (let m of moves) {
-        offset[0] += m[0]
-        offset[1] += m[1]
+        x += m[0]
+        y += m[1]
     }
-    let hash = `${offset[0]},${offset[1]}`
-    if (!tiles.hasOwnProperty(hash))
-        tiles[hash] = false
-    tiles[hash] = !tiles[hash]
+    let hash = getHash(x, y)
+    let v = tiles.get(hash)
+    tiles.set(hash, v === undefined ? true : !v)
 }
 
 let count = 0
-for (let t of Object.keys(tiles))
-    count += tiles[t] ? 1 : 0
+for (let v of tiles.values())
+    count += v ? 1 : 0
 
 console.log('Black', count)
 
 // Part two
 
+let nearOffset = [
+    [1, 0], [-1, 0], [1, -1], [0, 1], [0, -1], [-1, 1]
+]
+
+for (let i = 0; i < 100; i++) {
+    // add neighbours
+    let newHashes = []
+    for (let t of tiles.keys()) {
+        let xy = getXY(t)
+        for (let n of nearOffset) {
+            let hash = getHash(xy[0] + n[0], xy[1] + n[1])
+            if (!tiles.has(hash) && !newHashes.includes(hash))
+                newHashes.push(hash)
+        }
+    }
+    for (let h of newHashes)
+        tiles.set(h, false)
+
+    // apply rules
+    let next = new Map()
+    for (let kv of tiles.entries()) {
+        let black = 0
+        let xy = getXY(kv[0])
+        for (let n of nearOffset) {
+            let hash = getHash(xy[0] + n[0], xy[1] + n[1])
+            black += tiles.get(hash) ? 1 : 0
+        }
+        next.set(kv[0], kv[1] ? (black === 1 || black == 2) : black === 2)
+    }
+    tiles = next
+}
+
+count = 0
+for (let v of tiles.values())
+    count += v ? 1 : 0
+
+console.log('Black part two', count)
